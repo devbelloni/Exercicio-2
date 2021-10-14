af93191f7c4c09e9107ec45fb75e0f9fa62040f4
devbelloni <prof.belloni@gmail.com>
2021-10-13 23:38:38 -0300

    Alteradas

--- Exercicio 2/Program.cs
+++ Exercicio 2/Program.cs
@@ -23,10 +23,6 @@ namespace Exercicio_2 {
             Console.WriteLine(nom);
             Console.WriteLine(idade);
             Console.WriteLine(altura.ToString("F2", CultureInfo.InvariantCulture));
-            
-            % teste
-
-
         }
     }
 }
